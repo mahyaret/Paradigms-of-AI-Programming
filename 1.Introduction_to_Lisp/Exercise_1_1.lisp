4db9893511ed80c0b914e0a66bdae618ada586a5
mahyar.etedal@icloud.com <mahyar.etedal@icloud.com>
2019-02-06 19:40:45 -0500

    first Exercise added

--- 1.Introduction_to_Lisp/Exercise_1_1.lisp
+++ 1.Introduction_to_Lisp/Exercise_1_1.lisp
@@ -1,3 +1,6 @@
+(defparameter *titles* '(Mr Mrs Miss Ms Sir Madam Dr Admiral Major General MD\, Jr.))
 (defun last-name (name)
   "This function returns the last name"
-  (rest name))
+  (if (member (first (last name)) *titles*)
+      (last-name (reverse (rest (reverse name))))
+      (first (last name))))
