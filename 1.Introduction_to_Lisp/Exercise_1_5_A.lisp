4a251491ca600b17ea4c3f70522ebb6fb49ba409
mahyar.etedal@icloud.com <mahyar.etedal@icloud.com>
2019-02-14 21:21:25 -0500

    Second implementation is added

---
+++ 1.Introduction_to_Lisp/Exercise_1_5_A.lisp
@@ -0,0 +1,6 @@
+(defun dot-product (a b)
+  "Compute the mathematical dot product of two vectors."
+  (if (or (null a) (null b))
+      0
+      (+ (* (first a) (first b))
+	 (dot-product (rest a) (rest b)))))
